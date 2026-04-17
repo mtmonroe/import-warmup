9c7e74760b43ea1d3414387f2b23fa0d117c2378
Michael Monroe <74122154+mtmonroe@users.noreply.github.com>
2026-04-17 16:28:16 +0000

    push

--- run_exercises.py
+++ run_exercises.py
@@ -1,2 +1,5 @@
+import portfolio.data
+import portfolio.report
+from portfolio.assets import make_asset
 my_portfolio = portfolio.data.create_portfolio("Retirement")
 portfolio.report.print_report(my_portfolio)
